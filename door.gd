aa63b94a34d10344e8c9f0bd8046afce2360b153
h19BeMa <59867943+h19BeMa@users.noreply.github.com>
2020-03-17 11:54:33 +0100

    Add files via upload
    försöker fixa logiken

--- door.gd
+++ door.gd
@@ -10,22 +10,21 @@ var dia_index = 1
 
 export (String, FILE, "*.json") var dialogue_file_path : String
 var dpath = "data/dialogue_door.json"
-var dpath_ohnoo = "data/dialogue_door_ohnoo.json"
 var dialogue : Dictionary
-var ohnoo : Dictionary
 
 func _ready():
 	sprite.show()
 	text_label.hide()
 	dialogue = load_dialogue(dpath)
-	ohnoo = load_dialogue(dpath_ohnoo)
-
+	
 func _input(event):
 	if Input.is_action_just_pressed("ui_accept") && dialog_over == true:
+		#öppna nästa scene
 		print("booh")
-	
 
 func interact():
+	#logiken är wack stämmer inte
+	
 	#ifall du har nyckel
 	if get_item.items["1"]["is_picked"] == true:
 		
@@ -39,7 +38,7 @@ func interact():
 				text_label.show()
 			
 				if dia_index != dialogue.size() + 1:
-					text_label.text = dialogue[str(dia_index)]["text"]
+					text_label.text = dialogue["1"][str(dia_index)]["text"]
 					dia_index += 1
 				else:
 					dia_index = 1
@@ -52,46 +51,22 @@ func interact():
 				text_label.visible == false
 				movement.SPEED = 0
 				text_label.show()
-				text_label.text = dialogue["1"]["text"]
+				text_label.text = dialogue["1"]["1"]["text"]
 				
 			else:
 				text_label.hide()
 				movement.SPEED = 200
 				dialog_over = true
-				
-	#ifall du inte har nyckel
-	else:
-		
-		#ifall du har gåt igenom dialogen en gång
-		if dialog_over == false:
-			
-			#ifall du har mer än en rad med dialog
-			if dialogue.size() > 1:
-				text_label.visible == false
-				movement.SPEED = 0
-				text_label.show()
-			
-				if dia_index != dialogue.size() + 1:
-					text_label.text = dialogue[str(dia_index)]["text"]
-					dia_index += 1
-				else:
-					dia_index = 1
-					text_label.hide()
-					movement.SPEED = 200
-					dialog_over = true
-					
-			#ifall storleken på dialogen är en rad
-			elif dialogue.size() == 1: 
-				text_label.visible == false
-				movement.SPEED = 0
-				text_label.show()
-				text_label.text = dialogue["1"]["text"]
-			
-			else:
-				text_label.hide()
-				movement.SPEED = 200
-				dialog_over = true
-		
+	
+	#ifall du inte har nyckel			
+	elif get_item.items["1"]["is_picked"] == false:
+		text_label.visible == false
+		movement.SPEED = 0
+		text_label.show()
+		text_label.text = dialogue["2"]["text"]
+		text_label.hide()
+		movement.SPEED = 200
+		dialog_over = true
 
 func load_dialogue(file_path) -> Dictionary:
 
