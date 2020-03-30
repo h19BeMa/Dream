145a681e045e94b786954de6b20b8ec4fa81d740
h19BeMa <59867943+h19BeMa@users.noreply.github.com>
2020-03-30 15:27:25 +0200

    fixade animationer förutom hopp, dash, slash och block

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -2,13 +2,13 @@ extends KinematicBody2D
 
 onready var get_item = get_node("../item_class")
 
-const GRAVITY = 5
+const GRAVITY = 10
 const JUMP = -250
 const FLOOR = Vector2(0, -1)
 
 var hp = 200
 var cooldown = false
-var DASH = 1000
+var DASH = 1
 var SPEED = 200
 var on_ground = false
 var dashed = true
@@ -16,6 +16,7 @@ var velocity = Vector2()
 var entered_key = false
 var entered_door = false
 var entered_sword = false
+var Dduration = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -32,25 +33,41 @@ func _process(delta):
 		if Input.is_action_pressed("slash"):
 			#vanlig attack
 			pass
-	
-	if Input.is_action_pressed("ui_right"):
-		velocity.x = SPEED
-		if get_item.items["3"]["is_picked"] == true:	
+			
+	if get_item.items["3"]["is_picked"] == true:	
+		if Input.is_action_just_pressed("dash"):
 			if cooldown == true:
-				if Input.is_action_just_pressed("dash"):
-					velocity.x = SPEED + DASH 
-					cooldown = false
+				DASH = 3
+				cooldown = false
+			else:
+				DASH = 1
+				cooldown = false
+
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = SPEED * DASH
+		if get_item.items["3"]["is_picked"] == true:
+			$CollisionShape2D/Sprite.flip_h = false
+			$CollisionShape2D/Sprite.play("walking_with_sword_R")
+		else:
+			$CollisionShape2D/Sprite.flip_h = true
+			$CollisionShape2D/Sprite.play("walking")
+		
 
 	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -SPEED
+		velocity.x = -SPEED * DASH
 		if get_item.items["3"]["is_picked"] == true:
-			if cooldown == true:
-				if Input.is_action_pressed("dash"):
-					velocity.x = -SPEED + -DASH
-					cooldown = false
+			$CollisionShape2D/Sprite.flip_h = false
+			$CollisionShape2D/Sprite.play("walking_with_sword_L")
+		else:
+			$CollisionShape2D/Sprite.flip_h = false
+			$CollisionShape2D/Sprite.play("walking")
 
 	else:
 		velocity.x = 0 
+		if get_item.items["3"]["is_picked"] == true:
+			$CollisionShape2D/Sprite.play("default_with_sword")
+		else:
+			$CollisionShape2D/Sprite.play("default")
 
 	if Input.is_action_pressed("ui_up"):
 		if on_ground == true:
@@ -74,32 +91,22 @@ func _process(delta):
 	if Input.is_action_just_pressed("ui_accept") && entered_sword == true:
 		get_node("../items/sword").interact()
 
-
 func _on_Area2D_body_entered(body):
 	entered_key = true
-
 func _on_Area2D_body_exited(body):
 	entered_key = false
 
-
 func _on_door_body_entered(body):
 	entered_door = true
-
 func _on_door_body_exited(body):
 	entered_door = false
 
-
 func _on_sword_body_entered(body):
 	entered_sword = true
-
 func _on_sword_body_exited(body):
 	entered_sword = false
 
-
 func _on_Timer_timeout():
 	cooldown = true
-	
-
-
 
 
